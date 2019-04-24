9341c0ecfad2725194e42bda0b2b8cfc96d77f42
Christher Lenander <christher.lenander@schotte.se>
2019-04-24 21:57:27 +0200

    added helmet and compression.

--- exercises/workout.model.js
+++ exercises/workout.model.js
@@ -9,4 +9,4 @@ const schema = new Schema({
 
 schema.set('toJSON', { virtuals: true });
 
-module.exports = mongoose.model('Workout', schema);
+module.exports = mongoose.model('Workout', workout);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('rootpath')();
 const compression = require('compression');
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -9,9 +10,17 @@ const errorHandler = require('_helpers/error-handler');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(compression());
+
 app.use(cors());
 
-app.use(compression());
+app.use(helmet({
+  frameguard: {
+    action: 'deny'
+  },
+  noCache: true
+}));
+
 
 // use JWT auth to secure the api
 app.use(jwt());
@@ -24,7 +33,7 @@ app.use('/workouts', require('./workouts/workouts.controller'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3000;
 const server = app.listen(port, function () {
-    console.log('Server listening on port ' + port);
+  console.log('Server listening on port ' + port);
 });
